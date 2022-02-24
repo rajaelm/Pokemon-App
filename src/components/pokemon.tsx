5436fa206987e2cdb50993d56989b64187bb4227
rajaelm <lamaibdel.rajae@gmail.com>
2022-02-24 10:37:50 +0100

    fix modal

--- src/components/pokemon.tsx
+++ src/components/pokemon.tsx
@@ -1,66 +1,44 @@
-
 import axios from "axios";
 import { FC } from "react";
 import { useQuery } from "react-query";
 
-
 interface PkmnStats {
-	spriteUrl: string;
-	name: string;
-	baseattack: number;
-	
-	
-
+  spriteUrl: string;
+  name: string;
+  baseattack: number;
 }
 
 const formatStats = (data: any): PkmnStats => {
-	
-		
-	return {
-		name: data.data.name,
-		spriteUrl: data.data.sprites.front_default,
-		baseattack:data.data.sprites.stats,
-	
-        }
+  return {
+    name: data.data.name,
+    spriteUrl: data.data.sprites.front_default,
+    baseattack: data.data.sprites.stats,
+  };
 };
-
 interface Props {
-	url: string;
-	id : number;
+  id: number;
 }
 
-const Pokemon: FC<Props> = ( {url,id}) => {
-	
-	//const { data, isLoading, isError } = useQuery(['pokemon',url], fetchStats,{
-	const { data, isLoading, isError } = useQuery('pokemon', ()=>{
-		id++;
-		id =3;
-		url = 'https://pokeapi.co/api/v2/pokemon/'+ id;
-		console.log(url);
-		return axios.get(url)
-	})
+const Pokemon: FC<Props> = ({ id }) => {
+  //const { data, isLoading, isError } = useQuery(['pokemon',url], fetchStats,{
+  const { data, isLoading, isError } = useQuery("pokemon", () => {
+    id++;
+    //id =3;
+    const url = "https://pokeapi.co/api/v2/pokemon/" + id;
+    console.log(url);
+    return axios.get(url);
+  });
+  if (isLoading) return <p>Loading</p>;
+  if (isError) return <p>Error</p>;
 
-	if (isLoading) return <p>Loading</p>;
-	if (isError) return <p>Error</p>;
+  const stats = formatStats(data);
 
-	const stats = formatStats(data);
-	
-
-	return (
-		<>
-		
-		{ <img
-				className="sprite"
-				src={stats.spriteUrl}
-				alt={`${stats.name}`}
-				/> }
-          <div>
-		  {/* <p className="name">{stats.name}</p> */}
-		  
-          </div>
-		</>
-	);
+  return (
+    <>
+      {<img className="sprite" src={stats.spriteUrl} alt={`${stats.name}`} />}
+      <div>{<p className="name">{stats.name}</p>}</div>
+    </>
+  );
 };
 
 export default Pokemon;
-
--- src/components/pokemonList.tsx
+++ src/components/pokemonList.tsx
@@ -1,99 +1,97 @@
-
- import {
-
-    useInfiniteQuery
-  } from "react-query";
-  import useIntersectionObserver from '../hooks/use-intersection-observer';
-  import { useRef } from "react";
-  import { fetchPokemon } from "./api";
-
-  import Box from '@material-ui/core/Box';
-
-import React,{Fragment} from "react";
+import { useInfiniteQuery } from "react-query";
+import useIntersectionObserver from "../hooks/use-intersection-observer";
+import { useRef, useState } from "react";
+import { fetchPokemon } from "./api";
+import Box from "@material-ui/core/Box";
+import React, { Fragment } from "react";
 import { Modal } from "@material-ui/core";
 import Pokemon from "./pokemon";
-function PokemonList(){
-
-    const {
-      data,
-      isLoading,
-      fetchNextPage,
-      hasNextPage,
-      isFetchingNextPage
-    } = useInfiniteQuery("pokemons", fetchPokemon, {
-  
-      getNextPageParam: (lastPage) => lastPage.nextPage
+function PokemonList() {
+  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteQuery("pokemons", fetchPokemon, {
+      getNextPageParam: (lastPage) => lastPage.nextPage,
     });
-    
-    const loadMoreBtn = useRef<HTMLButtonElement>(null);
-    useIntersectionObserver({
-          target: loadMoreBtn,
-          onIntersect: () => fetchNextPage(),
-          enabled: hasNextPage,
-      });
-      const [open, setOpen] = React.useState(false);
-      const handleOpen = () => setOpen(true);
-      const handleClose = () => setOpen(false);
-      const style = {
-        position: "absolute" as "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: 400,
-        bgcolor: "background.paper",
-        border: "2px solid #000",
-        boxShadow: 24,
-        p: 4
-      };
-    
-    return (
-      <>
-        {isLoading ? (
-          <p>loading...</p>
-        ) : (
-          <>
-            <section className="flex-container" >
-                {data?.pages.map((group,id:number) =><>
-                <Fragment key={id}>{
-                   group.response.map((pokemon: {name: string; url: string }) =>
-                  <>
-                   <div  className="card" >
-                    <main className="card__body" >
-                      <p>#{id} :</p>
-                      <br />
-                      <p className="name" key={pokemon.name} onClick={handleOpen}>{pokemon.name}</p>
-                      
-                      <Pokemon url={pokemon.url} id={id} />
-                      <Modal
-                        open={open}
-                        onClose={handleClose}
-                        aria-labelledby="Details"
-                        aria-describedby="modal-modal-description">
-                        <Box sx={style}>
-                             <Pokemon url={pokemon.url} id={id} /> 
-                        </Box>
-                    </Modal> 
-                    </main>                
-                  </div>               
-                  </>)}
-                      </Fragment></>
-                )}  
-            </section>
-            <button
-              className="load-more"
-             ref={loadMoreBtn}
-             onClick={() => fetchNextPage()}
-             disabled={!hasNextPage}
+
+  const loadMoreBtn = useRef<HTMLButtonElement>(null);
+  useIntersectionObserver({
+    target: loadMoreBtn,
+    onIntersect: () => fetchNextPage(),
+    enabled: hasNextPage,
+  });
+  const [pokeid, setPokeid] = useState(0);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = (id: number) => {
+    setPokeid(id);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          <section className="flex-container">
+            {data?.pages.map((group, id) => (
+              <>
+                <Fragment key={id}>
+                  {group.response.map(
+                    (pokemon: { name: string; url: string }) => (
+                      <>
+                        <div className="card" key={id}>
+                          <main
+                            className="card__body"
+                            onClick={() => handleOpen(id)}
+                          >
+                            <p>#{id} : </p>
+                            <br />
+                            <p className="name">{pokemon.name}</p>
+                          </main>
+                        </div>
+                      </>
+                    )
+                  )}
+                </Fragment>
+              </>
+            ))}
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="Details"
+              aria-describedby="Pokemon details"
             >
-              {isFetchingNextPage
-                ? "Loading more..."
-                : hasNextPage
-                ? "Load More"
-                : "Nothing more to load"}
-            </button>
-          </>
-        )}
-      </>
-    );
-  }
+              <Box sx={style}>
+                <Pokemon id={pokeid} />
+              </Box>
+            </Modal>
+          </section>
+          <button
+            className="load-more"
+            ref={loadMoreBtn}
+            onClick={() => fetchNextPage()}
+            disabled={!hasNextPage}
+          >
+            {isFetchingNextPage
+              ? "Loading more..."
+              : hasNextPage
+              ? "Load More"
+              : "Nothing more to load"}
+          </button>
+        </>
+      )}
+    </>
+  );
+}
 export default PokemonList;
